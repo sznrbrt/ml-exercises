19fa41a764bbfd317ebe1373db0d4f49813d6505
Norbert Szucs <szucs.norbert@outlook.com>
2017-07-25 17:16:10 +0200

    Complete gradientDescentMulti function

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,17 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    
+    % Simple solution
+    % predictions = X * theta;
+    % errors = (predictions - y);
+    % for idx = 1:size(X, 2)
+    %   theta(idx) = theta(idx) - alpha / m * sum((errors .* X(:, idx)));
+    % end
+   
+    % Vectorized solution
+    delta = (1/m) * sum(X .* repmat((X*theta - y), 1, size(X,2)));
+    theta = (theta' - (alpha * delta))';g
     % ============================================================
 
     % Save the cost J in every iteration    
